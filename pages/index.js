e0acc9dda84dd38a5c7e7681c90219d63368655d
mryash04 <yash.04may@gmail.com>
2020-11-30 16:37:09 +0530

    ArticlePreview

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,32 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import RedoIcon from '@material-ui/icons/Redo';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="px-8 mt-10">
       <Head>
-        <title>Create Next App</title>
+        <title>Article Preview App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="mx-auto lg:w-3/5 lg:flex lg:flex-row lg:h-auto">
+        <img className="rounded-tr-md rounded-tl-md h-40 w-full lg:h-auto lg:w-2/5 lg:rounded-bl-md lg:rounded-tr-none" src="articleimage.jpg" alt="article" />
+      </div>
+      <div className="bg-white p-8 rounded-tl-md rounded-br-md lg:rounded-bl-none lg:rounded-tr-md">
+        <h2 className="text-gray-700 font-semibold">Articles make it easy to create rich and beautiful content in Channels. They are designed for long news, blogs and all types of content that needs a rich medium to be expressed properly.</h2>
+        <p className="text-sm text-gray-600 mt-4">Articles are designed to be beautiful and easy to use. You have all the same content controls & formatting as you have in Pages, and in addition to that, you have easy to use image layouts, that make any boring article awesome 😄</p>
+        <div className="flex items-center mt-8">
+        <div className="flex items-center">
+          <img className="h-10 w-10 rounded-full" src="reader.jpg" alt="reader" />
+          <div className="ml-4">
+            <p className="text-gray-800 text-sm font-semibold">Mark Williamsoses</p>
+            <p className="text-gray-600 text-sm">27 Nov 2020</p>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className=" w-8 h-8 ml-auto bg-gray-200 rounded-full flex items-center justify-center">
+          <RedoIcon/>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
